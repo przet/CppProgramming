dd324d3b9ca0dbdc6ba9c5cf61a59b76cba5c45b
CrazyPenguin <p.rzetelski.note@gmail.com>
2017-11-17 12:35:36 +1030

    Created new folder for CodeWars. Inside this folder, created a src and bin folder. Committed src and bin for WIP indexedCapitalisation problem.

---
+++ CodeWars/src/codewars_indexedCapitalisation.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    std::string str = "abcdef";
+    std::vector <int> vec {1, 2, 5};
+    
+    for (int i = 0; i <= vec.size(); ++i) 
+    {
+	std::cout << "\n" << vec.size();
+	int ascii_zeroIndx = static_cast<int> (str[0]);
+	std::cout << "\n" << ascii_zeroIndx;
+
+
+
+
+
+    }   
+
+
+
+
+
+
+
+
+}
