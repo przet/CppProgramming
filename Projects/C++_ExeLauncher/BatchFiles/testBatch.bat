159074109270a5d1ebf4a5f154ca926e6aea5e87
Philippe <p.rzetelski.note@gmail.com>
2018-02-06 18:26:34 +1030

    -Clean up of repo -Header guards included in run it cpp -New header file for run it

--- Projects/C++_ExeLauncher/BatchFiles/testBatch.bat
+++ Projects/C++_ExeLauncher/BatchFiles/testBatch.bat
@@ -1,5 +1,6 @@
 :: just an empty batch file to be called from RunIt V2. Otherwise could be known:: as a mock entity.
-
-system("pause")  
+@echo off
+color 1a
+pause  
 
 ::TODO this is another vulnerability! Find another way!
--- Projects/C++_ExeLauncher/RunItv2.cpp
+++ Projects/C++_ExeLauncher/RunItv2.cpp
@@ -1,75 +1,8 @@
-#include <windows.h>
-#include <Shellapi.h>
-//TODO: Include header guards?
+#ifndef MYCLASS_H_V2
+#include "C++_ExeLauncher_VS\C++_ExeLauncher_VS\RunItV2.h"  //I think I should play around with the config manager to not have to include
+                                                            // the path?
+#endif 
 
-
-class ExecuteFile
-{
-    public:
-    //TODO Having this private resulted in access violotions in
-    // derived classes, comeback to this (I would want this to be
-    // private
-        _In_opt_ HWND hwnd = NULL;
-        _In_opt_ LPCTSTR lpOperation = NULL;
-        _In_     LPCTSTR lpFile = NULL;
-        _In_opt_ LPCTSTR lpParameters = NULL;
-        _In_opt_ LPCTSTR lpDirectory = NULL;
-        _In_     INT nShowCmd = NULL;
-    
-    public:
-        virtual void SetParamsAndRun(_In_opt_ HWND, _In_opt_ LPCTSTR,
-            _In_ LPCTSTR, _In_opt_ LPCTSTR,
-            _In_opt_ LPCTSTR, _In_ INT) = 0;
-            //TODO pass by reference didn't seems to work in the 
-            // function call (i.e when derived class functions used
-            // in main)
-
-};
-
-class RunBatch : public ExecuteFile
-{
-    public:
-        void SetParamsAndRun(_In_opt_ HWND, _In_opt_ LPCTSTR,
-            _In_ LPCTSTR, _In_opt_ LPCTSTR,
-            _In_opt_ LPCTSTR, _In_ INT) override;
-
-};
-
-void RunBatch::SetParamsAndRun(_In_opt_ HWND hwndInput, _In_opt_ LPCTSTR
-    lpOperationInput, _In_ LPCTSTR lpFileInput, _In_opt_ LPCTSTR lpParametersInput,
-    _In_opt_ LPCTSTR lpDirectoryInput, _In_ INT nShowCmdInput)
-{
-    hwnd = hwndInput;
-    lpOperation = lpOperationInput;
-    lpFile = lpFileInput;
-    lpParameters = lpParametersInput;
-    lpDirectory = lpDirectoryInput;
-    nShowCmd = nShowCmdInput;
-    ShellExecute(hwnd, lpOperation, lpFile, lpParameters,
-        lpDirectory, nShowCmd);
-}
-class RunProgram : public ExecuteFile
-{
-    public:
-        void SetParamsAndRun(_In_opt_ HWND, _In_opt_ LPCTSTR,
-            _In_ LPCTSTR, _In_opt_ LPCTSTR,
-            _In_opt_ LPCTSTR, _In_ INT) override;
-
-};
-
-void RunProgram::SetParamsAndRun(_In_opt_ HWND hwndInput, _In_opt_ LPCTSTR
-    lpOperationInput, _In_ LPCTSTR lpFileInput, _In_opt_ LPCTSTR lpParametersInput,
-    _In_opt_ LPCTSTR lpDirectoryInput, _In_ INT nShowCmdInput)
-{
-    hwnd = hwndInput;
-    lpOperation = lpOperationInput;
-    lpFile = lpFileInput;
-    lpParameters = lpParametersInput;
-    lpDirectory = lpDirectoryInput;
-    nShowCmd = nShowCmdInput;
-    ShellExecute(hwnd, lpOperation, lpFile, lpParameters,
-        lpDirectory, nShowCmd);
-}
 int main()
 {
     RunBatch runBatch;
