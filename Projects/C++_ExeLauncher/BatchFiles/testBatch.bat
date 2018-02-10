18cd90c09faf7f0926810c450684647262ef7cdd
Philippe <p.rzetelski.note@gmail.com>
2018-02-10 19:19:50 +1030

    Added a test batch file

--- Projects/C++_ExeLauncher/BatchFiles/testBatch.bat
+++ Projects/C++_ExeLauncher/BatchFiles/testBatch.bat
@@ -1,6 +1,8 @@
 :: just an empty batch file to be called from RunIt V2. Otherwise could be known:: as a mock entity.
 @echo off
 color 1a
+set MECDIR = C:\Windows
+set DIR_LINE_NOT_TOBE_READ_BY_RUNIT = C:\temp
 pause  
 
 ::TODO this is another vulnerability! Find another way!
