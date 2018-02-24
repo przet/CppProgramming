ab12ed7b96d0cd2dcefe93401a5e0c894cc7f03e
Philippe <p.rzetelski.note@gmail.com>
2018-02-24 16:34:23 +1030

    accumulating strings

--- BeautifulCPPKateGregory/accumulate.cpp
+++ BeautifulCPPKateGregory/accumulate.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <numeric> //!!NOTE. accumulate in the numeric, not algorithm, header
 #include <vector>
-
+#include <string>
 int main ()
 {
 		std::vector<int> a {1,2,3,4,5};
@@ -44,12 +44,39 @@ int main ()
 		std::cout << (total == 12345 ) << std::endl; //!Argument order in Lambda's matter! Here i is acting like total, the thing coming in)
 		total = std::accumulate(begin(a), end(a),12345,[]( auto i,auto total){return total;});
 		std::cout << (total == 5 ) << std::endl; 
+		//won't work: need two arguments to lamba
+		//(unless you can do something with capture list, that
+		//I do not know about yet
+		//total = std::accumulate(begin(a), end(a),12345,[](auto blah){return blah;});
+		std::cout << (total == 12345 ) << std::endl; 
 		a.push_back(198);
 		total = std::accumulate(begin(a), end(a),12345,[]( auto total,auto i){return i;});
 		std::cout << (total == 198 ) << std::endl; 
 		a.erase(--end(a));
 
 
+		//now some strings
+		std::vector<std::string> words{"one", "two", "three"};
+		auto allwords = std::accumulate(begin(words), end(words), std::string{});
+		std::cout << allwords << std::endl; //This is concatonation
+        
+		//Now some funky stuff!
+		allwords = std::accumulate(begin(words), end(words), std::string{"Words: "},
+						[](const std::string& total, std::string& s){return total + " " + s;});
+		std::cout << allwords << std::endl;
+
+		//Some notes: remember that the third argument to accumulate ("words" here) is
+		//the _starting value_ of accumulate, or more precisely the first value of the 
+		//the first argument of the lamda passed in. After which the the new value
+		//passed into the lamda argument total is (total + " " +s), and you can see
+		//what is going on. It has a very "recursive" style to it, even though its not recursion.
+		//Also not the use of actual type names in the lambda, other than auto. In this case I 
+		//prefer this as its helping me understand the lambda, but auto will be quicker once I am
+		//very comfortable. Maybe it could be also easier for someone else to read in review etc.
+		//Finally, the const in the string argument just seems to be there for protection, like in
+		//any funciton argument; removing it doesn't change anything
+		
+		
 
 
 
