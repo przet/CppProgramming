e75eb24d58b1685c6a854965a34fe4a3324c993a
Philippe <p.rzetelski.note@gmail.com>
2018-03-03 20:08:42 +1030

    Ablsolute include paths, and a error fix

--- CppUnitSetUp/test.cpp
+++ CppUnitSetUp/test.cpp
@@ -1,5 +1,6 @@
-#include "include/cppunit/extensions/HelperMacros.h"
+#include <cppunit/extensions/HelperMacros.h>
 #include "header.hpp"
+#include <iostream>
 
 class TestSource : public CppUnit::TestFixture
 {
@@ -11,7 +12,9 @@ class TestSource : public CppUnit::TestFixture
         void isMemberSet()
         {
             MyClass myclass;
+            
             CPPUNIT_ASSERT_EQUAL(2,myclass.get_a());
+            std::cout << myclass.get_a() << std::endl;
         }
 
 };
--- CppUnitSetUp/test_main.cpp
+++ CppUnitSetUp/test_main.cpp
@@ -1,5 +1,5 @@
-#include "include/cppunit/extensions/TestFactoryRegistry.h"
-#include "include/cppunit/ui/text/TextTestRunner.h"
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TextTestRunner.h>
 
 int main (int argc, char **argv)
 {
