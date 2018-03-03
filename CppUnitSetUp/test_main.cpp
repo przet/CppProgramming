554e93c9b8b28d7494141101d216f852d65c882d
Philippe <p.rzetelski.note@gmail.com>
2018-03-03 17:56:52 +1030

    extra additions to gitignore, and fixed minor error in test_main

--- CppUnitSetUp/test_main.cpp
+++ CppUnitSetUp/test_main.cpp
@@ -1,7 +1,7 @@
 #include "include/cppunit/extensions/TestFactoryRegistry.h"
 #include "include/cppunit/ui/text/TextTestRunner.h"
 
-int main (int argc, int **argv)
+int main (int argc, char **argv)
 {
     CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
     CppUnit::TextTestRunner runner;
