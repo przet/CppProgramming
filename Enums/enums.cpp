1cd2303afd7c07479b6a33e705af5e2882c0930f
CrazyPenguin <p.rzetelski.note@gmail.com>
2018-04-06 11:41:50 +0930

    More scoped enum stuff

--- Enums/enums.cpp
+++ Enums/enums.cpp
@@ -22,6 +22,13 @@ int main()
     mES = MyEnum_scoped::SomethingElse_Scoped;  //O.K.
     std::cout << static_cast<int>(mES) << std::endl;   //NOT explicitly casting to an int cast will not work like in unscoped enums
 
+    void printEnum_Scoped(MyEnum_scoped);  //needs to be const - otherwise compiler will compain the non-const lval reference
+                                                  // to type 'MyEnum_Scoped' cannot bind to a temporary of type 'MyEnum_scoped'.
+                                                  //DOES not need to be a reference type though - in fact, then it does NOT need to be const
+    
+    printEnum_Scoped(mES);
+    printEnum_Scoped(MyEnum_scoped::Something_Scoped);
+
     
     
     
@@ -36,6 +43,11 @@ int main()
 void printEnum(MyEnum_1 myEnumInstance)
 {
     std::cout << myEnumInstance << std::endl;
+}
+
+void printEnum_Scoped(MyEnum_scoped myEnum_scopedInstance) //needs to be const if using rerefence type - see comment at decleration inside main()
+{
+    std::cout << static_cast<int>(myEnum_scopedInstance) << std::endl;
 }
 
     
