1ac33b9edb3e47282eb2acb0454d72575e94260b
Philippe <p.rzetelski.note@gmail.com>
2018-04-09 20:39:44 +0930

    std::function and template play

--- FunctionalHeader/stdFunction_TemplateArgs.cpp
+++ FunctionalHeader/stdFunction_TemplateArgs.cpp
@@ -1,13 +1,32 @@
 #include <functional>
 #include <iostream>
 
-template <typename T, typename U>
-bool isTrue(T t, U u)
+//template <typename T, typename U>
+int aFunction(int a,int b,std::function <bool(double t, long u) >f)
 {
-    if (t < u)
+    if (f)
+        return a+b;
+    return 0;
+}
+
+bool aBoolFunc(double c, long d)
+{
+    if(c >4 || d>4)
         return true;
     return false;
 }
+
+int main()
+{
+    auto blah = std::bind(aBoolFunc,5,4);
+    aFunction(1,2,blah);
+
+    return 0;
+}
+
+    
+
+
 
 
 
