04ec38f07cd13caa6d45fc396ae110dbfc6156d5
philippe <p.rzetelski.note@gmail.com>
2018-10-17 11:55:13 +1030

    Code wars index Cap

--- CodeWars/src/codewars_indexedCapitalisation.cpp
+++ CodeWars/src/codewars_indexedCapitalisation.cpp
@@ -6,19 +6,24 @@ int main()
 {
     std::string str = "abcdef";
     std::vector <int> vec {1, 2, 5};
+
+    std::cout << str[0];
+    str[0] = 'h';
+    std::cout << str[0];
     
-    for (int i = 0; i <= vec.size(); ++i) 
+    
+    /*for (int i = 0; i <= vec.size(); ++i) 
     {
-	std::cout << "\n" << vec.size();
+	//std::cout << "\n" << vec.size();
 	int ascii_zeroIndx = static_cast<int> (str[0]);
-	std::cout << "\n" << ascii_zeroIndx;
+	//std::cout << "\n" << ascii_zeroIndx;
 
 
 
 
 
     }   
-
+	*/
 
 
 
