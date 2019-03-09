64eef05fb726ff58dcf85d1e2653cb247a37e233
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2019-03-09 23:30:20 +1030

    Changes for testing

--- Algorithms&DataStructures/AlgorithmsAndDataStructures/DriverInputData/SampleSpaceGenerator.cpp
+++ Algorithms&DataStructures/AlgorithmsAndDataStructures/DriverInputData/SampleSpaceGenerator.cpp
@@ -6,13 +6,17 @@
 namespace sample_space
 {
     typedef std::vector<int> Vec;
-    typedef std::vector<std::vector<int>> VecOfVec;
+    typedef std::vector<Vec> VecOfVec;
+
+    Vec v1, v2;
+    VecOfVec tableu{ v1,v2 };
+    
 
     void SampleSpaceGenerator::generator(const int sampleSize)
     {
         table.resize(sampleSize);
         std::for_each(begin(table), end(table), [i = 1](auto& elem) mutable{elem.resize(i++);
-        std::generate(begin(elem), end(elem), [i = 0]() mutable{return i++; }); });
+        std::generate(begin(elem), end(elem), [i = 0]() mutable{return ++i; }); });
     }
 
 }
