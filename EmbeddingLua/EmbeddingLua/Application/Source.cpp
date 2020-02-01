9c12325e5c66665a3aad22eb6b5b685b679a1f02
Philippe <p.rzetelski.note@gmail.com>
2020-02-01 19:09:23 +1030

    Lua math (and more) libs, and demonstration of Lua VM persistence
    This commit also extracts the checking of Lua input into a function. I have placed a TODO (question) comment also.

--- EmbeddingLua/EmbeddingLua/Application/Source.cpp
+++ EmbeddingLua/EmbeddingLua/Application/Source.cpp
@@ -26,34 +26,58 @@ extern "C"
 #pragma comment(lib, "C:\\Consilium\\MSGuildTalk2020\\libs\\lua-5.3.5_Win64_dll14_lib\\lua53.lib")
 #endif
 
+// Utility function declarations
+bool checkLuaInput(lua_State* luaVM, int retCode); 
+
 int main(int argc, char** argv)
 {
     // TODO: file!
-    std::string cmd = "a = 7 + 11";
+    std::string cmd = "a = 7 + 11 + math.sin(23.7)";
 
-    // Create Lua VM
+    // Create Lua VM (persistent)
+    // TODO: Q: what cleans it up? Are we placing it on the heap?
     lua_State* L = luaL_newstate();
+    luaL_openlibs(L); // includes math
 
     // note c_str: in C land here 
     int r = luaL_dostring(L, cmd.c_str());
 
     // For any embedding - need to have checks!!
-    if (r == LUA_OK)
+    if(checkLuaInput(L, r))
     {
-        // global to vm 
         lua_getglobal(L, "a");
-        // -1 top of lua stack
         if (lua_isnumber(L, -1))
         {
             float a_in_cpp = (float)lua_tonumber(L, -1);
             printToConsole("a in cpp =  ", a_in_cpp);
         }
+     }
 
-    }
+    // demonstration of Lua VM persitence
+    cmd = "a = a + 100";
+    r = luaL_dostring(L, cmd.c_str());
+
+    // TODO extract this repeated code into a function
+    if(checkLuaInput(L, r))
+    {
+        lua_getglobal(L, "a");
+        if (lua_isnumber(L, -1))
+        {
+            float a_in_cpp = (float)lua_tonumber(L, -1);
+            printToConsole("a in cpp =  ", a_in_cpp);
+        }
+     }
+    return 0;
+}
+
+// Utility function definitions
+bool checkLuaInput(lua_State* luaVM, int retCode)
+{
+    if (retCode == LUA_OK)
+        return true;
     else
     {
-        printToConsole(lua_tostring(L, -1));
+        printToConsole(lua_tostring(luaVM, -1));
+        return false;
     }
-
-    return 0;
 }
