017d2ee81fb17387fbb6360674799d756becca42
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2020-02-04 12:18:27 +1030

    Call Lua function to get specified database entry

--- EmbeddingLua/EmbeddingLua/Application/Source.cpp
+++ EmbeddingLua/EmbeddingLua/Application/Source.cpp
@@ -31,14 +31,6 @@ bool checkLuaInput(lua_State* luaVM, int retCode);
 
 int main(int argc, char** argv)
 {
-    struct Player
-    {
-        std::string title;
-        std::string name;
-        std::string family;
-        int level = 0;
-    } player;
-
     // Create Lua VM (persistent)
     // TODO: Q: what cleans it up? Are we placing it on the heap?
     lua_State* L = luaL_newstate();
@@ -48,20 +40,23 @@ int main(int argc, char** argv)
 
     if (checkLuaInput(L, r))
     {
-        lua_getglobal(L, "AddStuff");
+        lua_getglobal(L, "GetPlayer");
         if (lua_isfunction(L, -1))
         {
-            lua_pushnumber(L, 3.5f);
-            lua_pushnumber(L, 7.1f);
+			// Player at 0th location
+            lua_pushnumber(L, 1);
 
             // note we need to pass in 2, the number of arguments to function
             // and the number of arguments we are expecting as output (1)
             // lua_pcall executes the function
-            if (checkLuaInput(L, lua_pcall(L, 2, 1, 0)))
+            if (checkLuaInput(L, lua_pcall(L, 1, 1, 0)))
             {
-                // we told lua we are providing 2 args, so it
-                // pops 2 elems from stack and replaces with result
-                printToConsole("C++ called lua AddStuff(3.5f,7.1f) = ", (float)lua_tonumber(L, -1));
+				if (lua_istable(L, -1))
+				{
+					lua_pushstring(L, "Name");
+					lua_gettable(L, -2);
+					printToConsole("Name: ", lua_tostring(L, -1));
+				}
 
 
             }
--- EmbeddingLua/EmbeddingLua/LuaScript.lua
+++ EmbeddingLua/EmbeddingLua/LuaScript.lua
@@ -1,19 +1,18 @@
--- function time!
--- Table
-player = {}
-player["Title"] = "master"
-player["Name"] = "wilberforce"
-player["Family"] = "paladin"
-player["Level"] = 4
+players = {}
+players[0] = {Title = "master", Name = "wilberforce", Family = "paladin", Level = 4}
+players[1] = {Title = "student", Name = "henry the young", Family = "barbarian", Level = 1}
 
--- Alternatively:
--- player = {Title = "master",...etc}
 
 function AddStuff(a, b)
     -- print will use std::cout apparently
     print("[LUA] AddStuff("..a..", "..b..") called \n")
     return a + b 
 end
+
+function GetPlayer(n)
+	return players[n]
+end
+
 
 
 
