2f861ecce2d9af18bfa9d50bb4d1d01e395567bc
Philippe <p.rzetelski.note@gmail.com>
2020-04-25 19:24:45 +0930

    larger free store allocations and 8.1 sdk
    8.1 sdk because I deleted 10!

--- MemoryLeaks/LeakDetection/VS_Native_LD/MemoryLeak.cpp
+++ MemoryLeaks/LeakDetection/VS_Native_LD/MemoryLeak.cpp
@@ -1,10 +1,9 @@
 void func1()
 {
-    int* ptr = new int(4);
-    int* ptr2 = new int(5);
-    //int* ptr3 = new int(6);
-    delete ptr;
-    delete ptr2;
+    int* ptr = new int[100000000];
+    int* ptr2 = new int[100000000];
+    int* ptr3 = new int[100000000];
+    delete[] ptr;
     //delete ptr3;
 }
 
