95417a3078c02037f08d2da6cfb3f0164404534e
Philippe <p.rzetelski.note@gmail.com>
2021-01-01 19:21:02 +1030

    Modification to soln 12

--- CodeWars/src/WeirdWords/Solution12.cpp
+++ CodeWars/src/WeirdWords/Solution12.cpp
@@ -3,12 +3,8 @@
 std::string nextLetter(std::string str)
 {
     for (auto& elem : str)
-    {
-        if (isalpha(elem))
-        {
-            if (!isalpha(++elem))
-                elem = elem == '{'  ? 'a' : 'A';
-        }
-    }
+        if (isalpha(elem) && !isalpha(++elem))
+            elem = elem == '{'  ? 'a' : 'A';
+  
     return str;
 }
