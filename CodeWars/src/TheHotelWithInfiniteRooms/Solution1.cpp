4076af7a176d97cc0af1b7753bc8bf1bc50309f4
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2021-08-26 20:36:04 +0930

    Analytical solution

---
+++ CodeWars/src/TheHotelWithInfiniteRooms/Solution1.cpp
@@ -0,0 +1,20 @@
+long long int groupSize(long long int S,long long int D)
+{
+    long long n, priorSum, T;
+    if(S>1)
+    {
+        n = S - 1;
+        priorSum = n*(n+1)/2;
+        T = D + priorSum;
+        
+    }
+    else
+    {
+        priorSum = 0;
+        T = D;
+      
+    }
+    return ceil(S - 1 + ( (-1 + sqrt(1 + 8*T))/2 - (S-1)));
+    
+    
+}
