6e5a96aa17e63dca2cff1892be35dadb3c14ae1a
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2021-08-26 20:56:03 +0930

    Refactor
    Getting terser...

--- CodeWars/src/TheHotelWithInfiniteRooms/Solution1.cpp
+++ CodeWars/src/TheHotelWithInfiniteRooms/Solution1.cpp
@@ -1,15 +1,5 @@
 long long int groupSize(long long int S,long long int D)
 {
-    long long T;
-    if(S>1)
-    {
-        T = D + (S-1)*S/2;
-        
-    }
-    else
-    {
-        T = D;
-    }
+    long long T = S > 1? D + (S-1)*S/2 : D;
     return ceil((-1 + sqrt(1 + 8*T))/2);
-    
 }
