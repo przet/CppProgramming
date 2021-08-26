df406315702336c2ff04c3e99351a06a77acb5fb
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2021-08-26 20:44:15 +0930

    Refactor
    Define and set variables only where needed

--- CodeWars/src/TheHotelWithInfiniteRooms/Solution1.cpp
+++ CodeWars/src/TheHotelWithInfiniteRooms/Solution1.cpp
@@ -1,18 +1,16 @@
 long long int groupSize(long long int S,long long int D)
 {
-    long long n, priorSum, T;
+    long long T;
     if(S>1)
     {
-        n = S - 1;
-        priorSum = n*(n+1)/2;
+        long long n = S - 1;
+        long long priorSum = n*(n+1)/2;
         T = D + priorSum;
         
     }
     else
     {
-        priorSum = 0;
         T = D;
-      
     }
     return ceil((-1 + sqrt(1 + 8*T))/2);
     
