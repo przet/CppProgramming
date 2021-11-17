952c4271bb045ef9039f3dce6115a37a78433f63
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2021-11-17 21:07:28 +1030

    Single threaded process showing different thread and process id
    Run it to see (could not assert as could not cast from std::thread::id to int).

--- MultiThreading/Parallel and Concurrent Programming with C++/Part1/ParallelConcurrentCpp_1/Source.cpp
+++ MultiThreading/Parallel and Concurrent Programming with C++/Part1/ParallelConcurrentCpp_1/Source.cpp
@@ -2,22 +2,29 @@
 #include <thread>
 #include <assert.h>
 
-void CPU_Waster(int* pid)
+void CPU_Waster(int* pid, std::thread::id* tid)
 {
     
     printf("CPU Waster Process ID: %d\n", _getpid());
+    printf("CPU Waster Thread ID: %d\n", std::this_thread::get_id());
     *pid = _getpid();
-
-
+    *tid = std::this_thread::get_id();
 }
 
 int main()
 {
     int main_process_pid = _getpid();
+    std::thread::id main_tid = std::this_thread::get_id();
     printf("Main Process ID: %d\n", main_process_pid);
+    printf("Main Thread ID: %d\n", main_tid);
     
     int cpu_waster_pid = 0;
-    CPU_Waster(&cpu_waster_pid);
+    std::thread::id cpu_waster_tid;
+    CPU_Waster(&cpu_waster_pid, &cpu_waster_tid);
+
     assert(cpu_waster_pid == main_process_pid);
+    assert(cpu_waster_tid == main_tid);
+    
+    
     return 0;
 }
